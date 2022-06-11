cea0cee89bab8b4b0f3ce5beecacb21f7af36d70
naveenG123 <106591480+naveenG123@users.noreply.github.com>
2022-06-11 22:55:23 +0530

    Add files via upload

---
+++ InventoryManagement/Startup.cs
@@ -0,0 +1,76 @@
+﻿using InventoryManagement.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<InventoryDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddTransient<IInventoryRepository, InventoryRepository>();
+            services.AddMvc();
+            var authenticationProviderKey = "TestKey";
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = authenticationProviderKey;
+            })//JWT Bearer
+                .AddJwtBearer(authenticationProviderKey, o =>
+                {
+                    var key = Encoding.UTF8.GetBytes(Configuration["JWT:Key"]);
+                    o.SaveToken = true;
+                    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidAudience = Configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+           
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(
+               options => options.WithOrigins("*")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+           );
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+           
+            app.UseMvc();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
