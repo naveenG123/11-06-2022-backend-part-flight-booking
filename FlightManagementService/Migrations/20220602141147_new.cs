dd4299f193ded0785082cae69f8ec9800faa96c8
naveenG123 <106591480+naveenG123@users.noreply.github.com>
2022-06-11 22:53:09 +0530

    Add files via upload

---
+++ FlightManagementService/Migrations/20220602141147_new.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlightManagementService.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_inventoryTbls",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "FlightNumber",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "AirlineNo",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "InstrumentUsed",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "TicketCost",
+                table: "inventoryTbls");
+
+            migrationBuilder.RenameColumn(
+                name: "ScheduleDays",
+                table: "inventoryTbls",
+                newName: "FlightNo");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NonBusinessClassSeat",
+                table: "inventoryTbls",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NoOfRows",
+                table: "inventoryTbls",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Meal",
+                table: "inventoryTbls",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BusinessClassSeat",
+                table: "inventoryTbls",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "inventoryTbls",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "inventoryTbls",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_inventoryTbls",
+                table: "inventoryTbls",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_inventoryTbls",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "inventoryTbls");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "inventoryTbls");
+
+            migrationBuilder.RenameColumn(
+                name: "FlightNo",
+                table: "inventoryTbls",
+                newName: "ScheduleDays");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NonBusinessClassSeat",
+                table: "inventoryTbls",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NoOfRows",
+                table: "inventoryTbls",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Meal",
+                table: "inventoryTbls",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BusinessClassSeat",
+                table: "inventoryTbls",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FlightNumber",
+                table: "inventoryTbls",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AirlineNo",
+                table: "inventoryTbls",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstrumentUsed",
+                table: "inventoryTbls",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TicketCost",
+                table: "inventoryTbls",
+                type: "decimal(8,2)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_inventoryTbls",
+                table: "inventoryTbls",
+                column: "FlightNumber");
+        }
+    }
+}
---
+++ FlightManagementService/Startup.cs
@@ -0,0 +1,85 @@
+﻿using FlightManagementService.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlightManagementService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<AirlineDBContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddTransient<IAirlineRepository, AirlineRepository>();
+            services.AddMvc();
+            var authenticationProviderKey = "TestKey";
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = authenticationProviderKey;
+            })//JWT Bearer
+                .AddJwtBearer(authenticationProviderKey, o =>
+                {
+                    var key = Encoding.UTF8.GetBytes(Configuration["JWT:Key"]);
+                    o.SaveToken = true;
+                    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JWT:Issuer"],
+                        ValidAudience = Configuration["JWT:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public  void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            app.UseCors(
+                options => options.WithOrigins("*")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+            );
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
+            
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
